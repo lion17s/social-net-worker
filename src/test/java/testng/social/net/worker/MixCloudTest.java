d1febb1a0d0054002a83657c6900bb4126c9f337
lion17s <serhii.sheptytskyi@gmail.com>
2023-03-25 22:05:23 +0200

    Update MixCloudTest.java

--- src/test/java/testng/social/net/worker/MixCloudTest.java
+++ src/test/java/testng/social/net/worker/MixCloudTest.java
@@ -12,7 +12,7 @@ public class MixCloudTest extends BaseUITest {
     @DataProvider(parallel = true)
     private static Object[][] showProvider() {
         return new Object[][]{
-                {"/progressive-mix-14/"}
+                {"/progressive-mix-15/"}
         };
     }
 
